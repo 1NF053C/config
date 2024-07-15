65ab254eee79dad1f6b45a1c0cf2f287ae80f0eb
c <wip>
2024-07-15 09:35:22 -0400

    todo: review container img and app img paths

--- compute_starter/build.sh
+++ compute_starter/build.sh
@@ -1,3 +1,2 @@
 # run on host
-source ./environment.sh
-apptainer build -F --fakeroot ${XDG_DATA_HOME}/compute/compute.sif ${XDG_CONFIG_HOME}/compute/compute.def
+apptainer build -F --fakeroot ./compute.sif ./compute.def
